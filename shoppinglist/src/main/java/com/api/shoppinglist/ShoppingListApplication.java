aff6906b2797ea6d690c5653e4f2b7aefc5fe538
Brian Sankey <briansankey93@gmail.com>
2024-07-29 21:59:55 -0500

    Spring initializr

---
+++ shoppinglist/src/main/java/com/api/shoppinglist/ShoppingListApplication.java
@@ -0,0 +1,15 @@
+package com.api.shoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListApplication.class, args);
+
+
+	}
+
+}
---
+++ shoppinglist/src/test/java/com/api/shoppinglist/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
