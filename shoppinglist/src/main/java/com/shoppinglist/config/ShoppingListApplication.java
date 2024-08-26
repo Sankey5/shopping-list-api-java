2ff0d53eae3916fe9bee90f398e0367d416a8212
sankey5 <briansankey93@gmail.com>
2024-08-26 15:20:50 -0500

    Moved static files to the resources directory

--- shoppinglist/src/main/java/com/shoppinglist/config/ShoppingListApplication.java
+++ shoppinglist/src/main/java/com/shoppinglist/config/ShoppingListApplication.java
@@ -2,13 +2,41 @@ package com.shoppinglist.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.shoppinglist")
 public class ShoppingListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingListApplication.class, args);
 	}
+
+//	public class MvcConfiguration implements WebMvcConfigurer {
+//
+//    @Bean
+//    public ViewResolver getViewResolver(){
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        // TODO: Change views directory name
+//        resolver.setPrefix("/webapp/WEB-INF/views/");
+//        resolver.setSuffix(".jsp");
+//        return resolver;
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//    }
+//
+//    @Bean
+//    RestTemplate restTemplate() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+//        converter.setObjectMapper(new ObjectMapper());
+//        restTemplate.getMessageConverters().add(converter);
+//        restTemplate.setErrorHandler(new RestResponseHandler());
+//        return restTemplate;
+//    }
+//}
 
 }
--- shoppinglist/src/main/webapp/resources/static/js/app.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-angular.module('Shopping_List_app',[]);
