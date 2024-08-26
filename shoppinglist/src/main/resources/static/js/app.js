4803368dfefc9b581e27d41fa9ec81b6bc46eacc
sankey5 <briansankey93@gmail.com>
2024-08-26 15:27:34 -0500

    Skeleton HTMX changes

--- shoppinglist/src/main/resources/static/js/app.js
+++ shoppinglist/src/main/resources/static/js/app.js
@@ -1,3 +1,15 @@
-'use strict';
+function toggleRecipeDropDown(event) {
+    let currentNode = event.currentTarget;
+    let carrotNode = currentNode.querySelector('.carrot');
+    let childTable = currentNode.querySelector('.grocery-item-table');
 
-angular.module('Shopping_List_app',[]);
+    toggleCarrotNode(carrotNode);
+
+    if(childTable) {
+        childTable.classList.toggle('not-visible');
+    }
+}
+
+function toggleCarrotNode(carrotNode) {
+    carrotNode.innerText = carrotNode.innerText == "⏴" ? "⏷" : "⏴";
+}
