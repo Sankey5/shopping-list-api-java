370dc752b4e2d03d5bd5257c3e050a29de822340
sankey5 <briansankey93@gmail.com>
2024-08-29 09:33:01 -0500

    Converted sql error handler to use logging

--- shoppinglist/src/main/java/com/shoppinglist/util/SQLExceptionHandler.java
+++ shoppinglist/src/main/java/com/shoppinglist/util/SQLExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.shoppinglist.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.SQLException;
 
 public class SQLExceptionHandler {
+    
+    public static final Logger LOGGER = LoggerFactory.getLogger(SQLExceptionHandler.class);
+
+    private SQLExceptionHandler() {}
 
     public static void handle(SQLException e) {
-        System.out.println("SQL Error code: " + e.getErrorCode());
-        System.out.println("SQL state: " + e.getSQLState());
-        System.out.println("Message: " + e.getMessage());
-        e.printStackTrace();
+        LOGGER.error(String.format("SQL Error code: %s", e.getErrorCode()));
+        LOGGER.error(String.format("SQL state: %s", e.getSQLState()));
+        LOGGER.error(String.format("Message: %s", e.getMessage()));
     }
 }
