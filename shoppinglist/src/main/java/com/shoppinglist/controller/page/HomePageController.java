0acb4c7d6d826b2c3d0d29e9b4e8ba1546d45290
sankey5 <briansankey93@gmail.com>
2024-09-03 15:52:27 -0500

    Resolved issue with 'Add recipe item' removing all elements

--- shoppinglist/src/main/java/com/shoppinglist/controller/page/HomePageController.java
+++ shoppinglist/src/main/java/com/shoppinglist/controller/page/HomePageController.java
@@ -2,6 +2,8 @@ package com.shoppinglist.controller.page;
 
 import com.shoppinglist.api.model.Recipe;
 import com.shoppinglist.api.service.RecipeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 @Controller
 public class HomePageController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(HomePageController.class);
 
     @GetMapping(value = "/")
     public String home() {
@@ -23,13 +29,23 @@ public class HomePageController {
     }
 
     @GetMapping(value = "/home/add-recipe")
-    public String recipeForm(@RequestParam(name = "recipeCount", defaultValue = "10") int recipeCount, Model model) {
+    public String recipeForm(@RequestParam(name = "recipeCount", required = false) Integer recipeCount, Model model) {
 
-        if(recipeCount > 25)
+        if(Objects.isNull(recipeCount)) {
+            recipeCount = 10;
+            model.addAttribute("start", 0);
+            model.addAttribute("entries", recipeCount);
+            return "addrecipe.html :: addRecipe";
+        }
+
+
+        if(recipeCount < 0 || recipeCount > 25)
             return "error";
 
-        model.addAttribute("entries", recipeCount);
-        return "addrecipe.html :: addRecipe";
+
+        model.addAttribute("start", recipeCount+1);
+        model.addAttribute("entries", recipeCount+1);
+        return "addrecipe.html :: singleRecipeInput";
     }
 
 }
