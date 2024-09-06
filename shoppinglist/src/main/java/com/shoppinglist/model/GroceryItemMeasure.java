887ac7a16e29c977fe9b76851be647084c0f83e5
sankey5 <briansankey93@gmail.com>
2024-09-06 10:37:11 -0500

    added constructor arguments to the groecery item enum

--- shoppinglist/src/main/java/com/shoppinglist/model/GroceryItemMeasure.java
+++ shoppinglist/src/main/java/com/shoppinglist/model/GroceryItemMeasure.java
@@ -6,23 +6,43 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum GroceryItemMeasure {
-    // Weight measurements
-    OUNCE, OZ,
-    POUND, LB, LBS,
+    // Dry measurements
+    OUNCE("dry", 1),
+    OZ("dry", 1),
+    POUND("dry", 16),
+    LB("dry", 16),
+    LBS("dry", 16),
 
     // Liquid measurements
-    TEASPOON, TSP,
-    TABLESPOON, TBSP,
-    CUP, C,
-    PINT, PT,
-    QUART, QT,
-    GALLON, GAL,
-
-    //Unusual
-    FLUID_OUNCE, FL_OZ,
+    TEASPOON("liquid",1.0/6.0),
+    TSP("liquid",1.0/6.0),
+    TABLESPOON("liquid",0.5),
+    TBSP("liquid",0.5),
+    CUP("liquid",8),
+    C("liquid",8),
+    PINT("liquid",16),
+    PT("liquid",16),
+    QUART("liquid",32),
+    QT("liquid",32),
+    GALLON("liquid",64),
+    GAL("liquid",64),
+    FLUID_OUNCE("liquid",1),
+    FL_OZ("liquid",1),
 
     //OTHER
-    NONE;
+    NONE("", 0);
+
+    final String measurementType;
+    final double ounces;
+
+    private GroceryItemMeasure(String measurementType, double ounces) {
+        this.measurementType = measurementType;
+        this.ounces = ounces;
+    }
+
+    public double getOunces() {return this.ounces;}
+
+    public String getMeasurementType() { return this.measurementType;}
 
     public static GroceryItemMeasure getGroceryItemMeasure(String val) throws IllegalArgumentException {
         return GroceryItemMeasure.valueOf(val.toUpperCase());
