9c3bab168e3ac44e0aeb46be96ae3191ec8f7010
sankey5 <briansankey93@gmail.com>
2024-09-10 16:47:25 -0500

    Changed groceryItemDAO interface signatures

--- shoppinglist/src/main/java/com/shoppinglist/api/dao/GroceryItemDAO.java
+++ shoppinglist/src/main/java/com/shoppinglist/api/dao/GroceryItemDAO.java
@@ -9,11 +9,11 @@ import java.util.List;
 public interface GroceryItemDAO {
     List<GroceryItem> getGroceryItemsForRecipe(long recipeId);
 
-    List<GroceryItem> saveGroceryItemsForRecipe(Connection connection, long recipeId, List<GroceryItem> updatedGroceryItems) throws SQLException;
+    List<GroceryItem> saveGroceryItemsForRecipe(long recipeId, List<GroceryItem> updatedGroceryItems) throws SQLException;
 
-    List<GroceryItem> updateGroceryItemsForRecipe(Connection connection, long recipeId, List<GroceryItem> updatedGroceryItems) throws SQLException;
+    List<GroceryItem> updateGroceryItemsForRecipe(long recipeId, List<GroceryItem> updatedGroceryItems) throws SQLException;
 
-    boolean deleteAllGroceryItemsForRecipe(Connection connection, long recipeId) throws SQLException;
+    boolean deleteAllGroceryItemsForRecipe(long recipeId) throws SQLException;
 
     boolean deleteGroceryItem(long groceryItemId);
 }
